2aab58aa63b86ba37c84c5e0451b8c81b6d6601c
AhamadRaza <54453256+AhamadRaza@users.noreply.github.com>
2019-08-24 01:02:47 +0530

    Create Student.java
    .java

---
+++ Student.java
@@ -0,0 +1,38 @@
+public class Student implements Serializable {
+  public static void main(String args[]){
+    private int sno;
+    private String sname;
+    private String semail;
+    private String smobile;
+    
+    public int getSno(){
+      return sno;
+    }
+    public void setSno(int no){
+      this.sno=sno;
+    }
+    
+    public String getSname(){
+      return sname;
+    }
+    public void setSname(String sname){
+      this.sname=sname;
+    }
+    
+    public String getSemail(){
+      return eemail;
+    }
+    public void setSemail(String semail){
+      this.semail=semail;
+    }
+    public String getSmobile(){
+      return smobil;
+    }
+    public void setSmobile(String smobile){
+      this.smobile=smobile;
+    }
+    
+  }
+
+
+}
